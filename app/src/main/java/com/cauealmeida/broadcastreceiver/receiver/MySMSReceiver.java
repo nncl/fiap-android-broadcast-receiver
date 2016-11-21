1d93d23a7175e74faa2d4c410b74356390921f57
Caue Bruno de Almeida <RM30917@fiap.com.br>
2016-11-21 20:16:06 -0200

    Adding SMS stuff - still need to be finished

---
+++ app/src/main/java/com/cauealmeida/broadcastreceiver/receiver/MySMSReceiver.java
@@ -0,0 +1,15 @@
+package com.cauealmeida.broadcastreceiver.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by RM30917 on 21/11/2016.
+ */
+public class MySMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO
+    }
+}
